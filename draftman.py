c09e5aad32c7d107a68e98909be2ac9774e7e8d2
jsseidel <jsseidel@fastmail.com>
2019-11-27 13:55:38 -0500

    Added icon and fixed directory of glade file

--- draftman.py
+++ draftman.py
@@ -2,13 +2,14 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
+import os
 import signal
 from handlers.AppWindowHandlerRouter import AppWindowHandlerRouter
 
 def main():
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     builder = Gtk.Builder()
-    builder.add_from_file('draftman2.glade')
+    builder.add_from_file('%s/draftman2.glade' % os.path.dirname(os.path.realpath(__file__)))
     builder.connect_signals(AppWindowHandlerRouter())
     app_window = builder.get_object('appWindow')
     app_window.show_all()
